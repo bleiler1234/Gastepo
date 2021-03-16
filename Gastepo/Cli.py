2c978ae23e60173f106d2fcfe26ab5d5dd91fdaa
yuzhonghua <583512498@qq.com>
2021-03-16 14:01:36 +0800

    更新命令行

--- Gastepo/Cli.py
+++ Gastepo/Cli.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
 import argparse
+import os
+import shutil
+
+
+def init_skeleton(args):
+    try:
+        shutil.copytree(os.path.dirname(os.path.abspath(__file__)), args.project)
+        print("[O(∩_∩)O]: Bingo!!! The project was initialized successfully, It is located on {}.".format(args.project))
+    except Exception:
+        print("[(o´ﾟ□ﾟ`o)]: Damn!!! An exception occurred while initializing the project directory.")
 
 
 def cli():
-    parser = argparse.ArgumentParser(prog="Gastepo", description="Gastepo Command Line",
-                                     epilog="Get Gastepo on GitHub [https://github.com/bleiler1234/gastepo]")
-    parser.add_argument("run", help="run test on gestepo")
-    parser.add_argument("-V", "--version", action="version", version='%(prog)s 1.0.0')
-    parser.parse_args()
+    parser = argparse.ArgumentParser(prog="Gastepo", description="%(prog)s Command Line",
+                                     epilog="Get %(prog)s on GitHub [https://github.com/bleiler1234/gastepo]")
+
+    parser.add_argument("-V", "--version", action="version", version='%(prog)s 0.0.1',
+                        help="show release version and exit")
+    parser.add_argument("start", help="Start with %(prog)s.")
+
+    subparsers = parser.add_subparsers(help="")
+
+    skeleton_parser = subparsers.add_parser("skeleton", help="Initialize project with %(prog)s Skeleton.")
+    skeleton_parser.add_argument("-p", "--project", nargs="?", help='specify project directory')
+    skeleton_parser.set_defaults(func=init_skeleton)
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
