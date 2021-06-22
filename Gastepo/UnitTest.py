56cc2dd75a8e17d286d8a019b1097078c49e40c2
yuzhonghua <tsl-yuzhonghua0914@tasly.com>
2021-06-22 22:46:23 +0800

    UnitTest

--- Gastepo/UnitTest.py
+++ Gastepo/UnitTest.py
@@ -4,31 +4,13 @@ import sys
 sys.path.append("/Users/mayer/Project/PycharmProjects/MileStone/Automation/Gastepo")
 
 import unittest
-
-from hamcrest import *
-
-from Gastepo.Core.Util.CommonUtils import is_number
-
-
-class CommonUtilsTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        print("set up class")
-
-    def setUp(self):
-        print("set up")
-
-    def test_is_number(self):
-        assert_that(is_number(3), is_(equal_to(True)))
-        print("test done")
-
-    def tearDown(self):
-        print("tear down")
-
-    @classmethod
-    def tearDownClass(cls):
-        print("tear down class")
-
+from Gastepo.QA.CommonUtilsTest import CommonUtilsTest
+from Gastepo.QA.ConfigUtilsTest import ConfigUtilsTest
 
 if __name__ == '__main__':
     unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(CommonUtilsTest)
+    suite.addTest(ConfigUtilsTest)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
